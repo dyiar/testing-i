c34c2468d4e38ef0cc0505f68819850ca36aecc9
dyiar <austin.hl.blake@gmail.com>
2019-01-28 16:51:11 -0700

    I think I finished testing for success and fail methods.

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,35 +1,65 @@
-const enhancer = require('../enhancer');
+const enhancer = require("../enhancer");
 
-describe('enhancer library', () => {
+describe("enhancer library", () => {
+  describe("success() method", () => {
+    const item = {
+      originalName: "BF Sword",
+      name: "BF Sword",
+      type: "weapon",
+      durability: 9,
+      enhancement: 15
+    };
 
-    describe('success() method', () => {
-        const item = {
-            originalName: "BF Sword",
-            name: "BF Sword",
-            type: 'weapon',
-            durability: 100,
-            enhancement: 'PEN'
-        };
+    const expected = {
+      originalName: "BF Sword",
+      name: "[PRI] BF Sword",
+      type: "weapon",
+      durability: 9,
+      enhancement: 'PRI'
+    };
 
-        const expected = {
-            originalName: "BF Sword",
-            name: "[PEN] BF Sword",
-            type: 'weapon',
-            durability: 100,
-            enhancement: 'PEN'
+    it("should give me back the same item", () => {
+      const actual = enhancer.success(item);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it("type should only be armor or weapon", () => {
+      expect(item.type).toBe("weapon" || "armor");
+    });
+
+    it('if enhancement is < 14, durability cant be below 25', () => {
+        if(item.enhancement < 14) {
+            expect(item.durability).not.toBeLessThan(25)
         }
-
-        it('should give me back the same item', () => {
-            const actual = enhancer.success(item);
-
-            expect(actual).toEqual(expected)
-        })
-
-        it('type should only be armor or weapon', () => {
-            expect(item.type).toBe('weapon' || 'armor')
-        })
-
     })
 
+    it('if enhancement is >15, durability cant be below 10', () => {
+        if(item.enhancement > 14 || NaN) {
+            expect(item.durability).not.toBeLessThan(10)
+        }
+    })
+  });
 
-})
+  describe('fail() method', () => {
+    const item = {
+        originalName: "BF Sword",
+        name: "BF Sword",
+        type: "weapon",
+        durability: 100,
+        enhancement: 'PEN'
+      };
+      const expected = {
+        originalName: "BF Sword",
+        name: "[TET] BF Sword",
+        type: "weapon",
+        durability: 90,
+        enhancement: 'TET'
+      };
+
+      it('should decrease durability by 5 if enhancement is between 0-14 and by 10 if between 15-PEN', () => {
+          const actual = enhancer.fail(item);
+          expect(actual).toEqual(expected)
+      })
+  })
+});
--- enhancer.js
+++ enhancer.js
@@ -1,44 +1,65 @@
 module.exports = {
-    success,
-    fail,
-    repair
-}
+  success,
+  fail,
+  repair
+};
 
 function success(item) {
-    const newItem = {
-        originalName: item.originalName,
-        name: `[+${item.enhancement+1}] ${item.originalName}`,
-        type: item.type,
-        durability: item.durability,
-        enhancement: item.enhancement+1
-    }
+  const newItem = {
+    originalName: item.originalName,
+    name: `[+${item.enhancement + 1}] ${item.originalName}`,
+    type: item.type,
+    durability: item.durability,
+    enhancement: item.enhancement + 1
+  };
 
-    if (item.enhancement === 15) {
-        newItem.name = `[PRI] ${item.originalName}`
-        newItem.enhancement = 'PRI'
-    } else if (item.enhancement === 'PRI') {
-        newItem.name = `[DUO] ${item.originalName}`
-        newItem.enhancement = 'DUO'
-    } else if (item.enhancement === 'DUO') {
-        newItem.name = `[TRI] ${item.originalName}`
-        newItem.enhancement = 'TRI'
-    } else if (item.enhancement === 'TRI') {
-        newItem.name = `[TET] ${item.originalName}`
-        newItem.enhancement = 'TET'
-    } else if (item.enhancement === 'TET' || item.enhancement === 'PEN') {
-        newItem.name = `[PEN] ${item.originalName}`
-        newItem.enhancement = 'PEN'
-    }
+  if (item.enhancement === 15) {
+    newItem.name = `[PRI] ${item.originalName}`;
+    newItem.enhancement = "PRI";
+  } else if (item.enhancement === "PRI") {
+    newItem.name = `[DUO] ${item.originalName}`;
+    newItem.enhancement = "DUO";
+  } else if (item.enhancement === "DUO") {
+    newItem.name = `[TRI] ${item.originalName}`;
+    newItem.enhancement = "TRI";
+  } else if (item.enhancement === "TRI") {
+    newItem.name = `[TET] ${item.originalName}`;
+    newItem.enhancement = "TET";
+  } else if (item.enhancement === "TET" || item.enhancement === "PEN") {
+    newItem.name = `[PEN] ${item.originalName}`;
+    newItem.enhancement = "PEN";
+  }
 
-    
-
-    return newItem;
+  return newItem;
 }
 
 function fail(item) {
+    const newItem = {
+        originalName: item.originalName,
+        name: `[+${item.enhancement}] ${item.originalName}`,
+        type: item.type,
+        durability: item.durability,
+        enhancement: item.enhancement
+      };
 
+      if (item.enhancement <= 14) {
+          newItem.durability = item.durability - 5
+      } else {
+          newItem.durability = item.durability -10
+      } 
+
+      if (item.enhancement === "DUO") {
+        newItem.name = `[PRI] ${item.originalName}`;
+        newItem.enhancement = "PRI";
+      } else if (item.enhancement === "TRI") {
+        newItem.name = `[DUO] ${item.originalName}`;
+        newItem.enhancement = "DUO";
+      } else if (item.enhancement === "PEN") {
+        newItem.name = `[TET] ${item.originalName}`;
+        newItem.enhancement = "TET";
+      }
+
+      return newItem
 }
 
-function repair(item) {
-
-}
+function repair(item) {}
